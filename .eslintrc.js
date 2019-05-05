1135995d5d5c492a8db459bd560a00028ba5b50a
Norbert Sram <me@norbertsram.com>
2019-05-05 13:55:40 +0200

    HOTFIX: use eslint instead of tslint for TypeScript as well

---
+++ .eslintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+  'env': {
+    'node': true,
+    'es6': true
+  },
+  parser:  '@typescript-eslint/parser',
+  'extends': [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended'
+  ],
+  'globals': {
+    'Atomics': 'readonly',
+    'SharedArrayBuffer': 'readonly'
+  },
+  'parserOptions': {
+    'ecmaVersion': 2018,
+    'sourceType': 'module'
+  },
+  'rules': {
+    "@typescript-eslint/indent": ["error", 2],
+    'indent': [
+      'error',
+      2
+    ],
+    'linebreak-style': [
+      'error',
+      'unix'
+    ],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'always'
+    ]
+  }
+};
