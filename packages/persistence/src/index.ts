aca41732be28384ee97bac926e3fd49d14261553
Norbert Sram <me@norbertsram.com>
2019-11-12 19:57:24 +0100

    Fixed default package entry.

--- packages/persistence/src/index.ts
+++ packages/persistence/src/index.ts
@@ -1,10 +1,2 @@
-const knex = require('knex')({
-  client: 'postgresql',
-  connection: 'postgres://6ar:6arpassword@localhost:5432/6ar'
-});
+export * from './repositories/border-traffic-repository';
 
-knex('border_traffic_entry').then((entries: Record<string, unknown>[]) => {
-  const recat = entries[0]['recorded_at'];
-  const creat = entries[0]['created_at'];
-  console.log(`Typeof ${recat instanceof Date} and ${typeof creat}`);
-});
