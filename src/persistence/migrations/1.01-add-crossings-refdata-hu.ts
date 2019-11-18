e55f2c5444ed43a9bf8a7b7e13993848d06aaae8
Norbert Sram <me@norbertsram.com>
2019-11-18 10:40:41 +0100

    Added missing crossing to ref data entries.

--- src/persistence/migrations/1.01-add-crossings-refdata-hu.ts
+++ src/persistence/migrations/1.01-add-crossings-refdata-hu.ts
@@ -25,12 +25,12 @@ const HU_BORDER_CROSSING_ENTRIES = [
   // Serbian crossings
   {'id': 17, 'name': 'Ásotthalom - Backi Vinogradi', 'foreign_country_code': 'RS', 'open_from':'0700', 'open_until': '1900'},
   {'id': 18, 'name': 'Bácsalmás - Bajmok', 'foreign_country_code': 'RS', 'open_from':'0700', 'open_until': '1900'},
-  {'id': 19, 'name': 'Bácsszentgyörgy – Raština', 'foreign_country_code': 'RS', 'open_from':'0700', 'open_until': '1900'},
+  {'id': 19, 'name': 'Bácsszentgyörgy - Raština', 'foreign_country_code': 'RS', 'open_from':'0700', 'open_until': '1900'},
   {'id': 20, 'name': 'Hercegszántó - Bački Breg', 'foreign_country_code': 'RS', 'open_from':'0000', 'open_until': '2400'},
   {'id': 21, 'name': 'Röszke - Horgoš autópálya', 'foreign_country_code': 'RS', 'open_from':'0000', 'open_until': '2400'},
   {'id': 22, 'name': 'Röszke - Horgoš közút', 'foreign_country_code': 'RS', 'open_from':'0700', 'open_until': '1900'},
   {'id': 23, 'name': 'Tiszasziget - Đala', 'foreign_country_code': 'RS', 'open_from':'0700', 'open_until': '1900'},
-  {'id': 24, 'name': 'Tompa - Kelebia', 'foreign_country_code': 'RS', 'open_from':'0000', 'open_until': '2400'},
+  {'id': 24, 'name': 'Tompa - Kelebija', 'foreign_country_code': 'RS', 'open_from':'0000', 'open_until': '2400'},
 
   // Crotaian crossings
   {'id': 25, 'name': 'Barcs - Terezino Polje', 'foreign_country_code': 'HR', 'open_from':'0000', 'open_until': '2400'},
@@ -44,17 +44,18 @@ const HU_BORDER_CROSSING_ENTRIES = [
   // Austrian crossings
   {'id': 32, 'name': 'Bozsok - Rechnitz', 'foreign_country_code': 'AT', 'open_from':'0000', 'open_until': '2400'},
   {'id': 33, 'name': 'Bucsu - Schachendorf', 'foreign_country_code': 'AT', 'open_from':'0000', 'open_until': '2400'},
-  {'id': 34, 'name': 'Fertőd-Pamhagen', 'foreign_country_code': 'AT', 'open_from':'0000', 'open_until': '2400'},
-  {'id': 35, 'name': 'Fertőrákos-Mörbisch', 'foreign_country_code': 'AT', 'open_from':'0000', 'open_until': '2400'},
-  {'id': 36, 'name': 'Hegyeshalom-Nickelsdorf autópálya', 'foreign_country_code': 'AT', 'open_from':'0000', 'open_until': '2400'},
-  {'id': 37, 'name': 'Jánossomorja-Andau', 'foreign_country_code': 'AT', 'open_from':'0000', 'open_until': '2400'},
-  {'id': 38, 'name': 'Kópháza-Deutschkreutz', 'foreign_country_code': 'AT', 'open_from':'0000', 'open_until': '2400'},
-  {'id': 39, 'name': 'Kőszeg - Rattersdorf', 'foreign_country_code': 'AT', 'open_from':'0000', 'open_until': '2400'},
-  {'id': 40, 'name': 'Pinkamindszent - Strem', 'foreign_country_code': 'AT', 'open_from':'0000', 'open_until': '2400'},
-  {'id': 41, 'name': 'Rábafüzes - Heiligenkreutz', 'foreign_country_code': 'AT', 'open_from':'0000', 'open_until': '2400'},
-  {'id': 42, 'name': 'Sopron-Klingenbach', 'foreign_country_code': 'AT', 'open_from':'0000', 'open_until': '2400'},
-  {'id': 43, 'name': 'Szentpéterfa - Eberau', 'foreign_country_code': 'AT', 'open_from':'0000', 'open_until': '2400'},
-  {'id': 44, 'name': 'Zsira-Lutzmannsburg', 'foreign_country_code': 'AT', 'open_from':'0000', 'open_until': '2400'}
+  {'id': 34, 'name': 'Fertőd - Pamhagen', 'foreign_country_code': 'AT', 'open_from':'0000', 'open_until': '2400'},
+  {'id': 35, 'name': 'Fertőrákos - Mörbisch', 'foreign_country_code': 'AT', 'open_from':'0000', 'open_until': '2400'},
+  {'id': 36, 'name': 'Hegyeshalom - Nickelsdorf autópálya', 'foreign_country_code': 'AT', 'open_from':'0000', 'open_until': '2400'},
+  {'id': 37, 'name': 'Hegyeshalom - Nickelsdorf közút (1-es főút)', 'foreign_country_code': 'AT', 'open_from':'0000', 'open_until': '2400'},
+  {'id': 38, 'name': 'Jánossomorja - Andau', 'foreign_country_code': 'AT', 'open_from':'0000', 'open_until': '2400'},
+  {'id': 39, 'name': 'Kópháza - Deutschkreutz', 'foreign_country_code': 'AT', 'open_from':'0000', 'open_until': '2400'},
+  {'id': 40, 'name': 'Kőszeg - Rattersdorf', 'foreign_country_code': 'AT', 'open_from':'0000', 'open_until': '2400'},
+  {'id': 41, 'name': 'Pinkamindszent - Strem', 'foreign_country_code': 'AT', 'open_from':'0000', 'open_until': '2400'},
+  {'id': 42, 'name': 'Rábafüzes - Heiligenkreutz', 'foreign_country_code': 'AT', 'open_from':'0000', 'open_until': '2400'},
+  {'id': 43, 'name': 'Sopron - Klingenbach', 'foreign_country_code': 'AT', 'open_from':'0000', 'open_until': '2400'},
+  {'id': 44, 'name': 'Szentpéterfa - Eberau', 'foreign_country_code': 'AT', 'open_from':'0000', 'open_until': '2400'},
+  {'id': 45, 'name': 'Zsira - Lutzmannsburg', 'foreign_country_code': 'AT', 'open_from':'0000', 'open_until': '2400'}
 ];
 
 export async function up(knex: Knex): Promise<void> {
