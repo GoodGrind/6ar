b52869d978f0e04eede8e94c75e3070d1e9c440b
MilanCsore <48695276+MilanCsore@users.noreply.github.com>
2020-04-22 13:10:38 +0200

    #14 raw html is saved (#25)
    * #14 unparsed data is saved in RAW HTML format at the same time as parsed data, to a different folder.
    
    * #14 CLI has been enhanced to save parsed and unparsed files at the same time to different places.

--- src/cli/cli.ts
+++ src/cli/cli.ts
@@ -1,26 +1,30 @@
-import { Crossings, startFetchTask } from '../police-hu';
+import {Crossings, RawCrossingData, startFetchTask} from '../police-hu';
 import fs from 'fs';
 import { promisify } from 'util';
-
 async function saveCrossingInformation(crossings: Crossings, toFile: string): Promise<void> {
   const JSON_INDENTATION = 2;
   const prettyPrinted = JSON.stringify(crossings, undefined, JSON_INDENTATION);
   const writeFile = promisify(fs.writeFile);
   return writeFile(toFile, prettyPrinted, 'utf8');
 }
-
-async function saveCurrentPoliceHuCrossingInfo(crossingInfo: Crossings, folder: string): Promise<boolean> {
+async function saveCrossingUnparsedInformation(crossings: RawCrossingData, toFile: string): Promise<void> {
+  const writeFile = promisify(fs.writeFile);
+  return writeFile(toFile,crossings);
+}
+async function saveCurrentPoliceHuCrossingInfo(crossingInfo: Crossings, folder: string, crossingUnparsedInfo: RawCrossingData, unparsedFolder: string): Promise<boolean> {
   const currentTime = new Date().toISOString();
+  const unparsedFileName = `${unparsedFolder}/police_hu_${currentTime}.html`;
   const fileName = `${folder}/police_hu_${currentTime}.json`;
   await saveCrossingInformation(crossingInfo, fileName);
+  await saveCrossingUnparsedInformation(crossingUnparsedInfo, unparsedFileName);
   return true;
 }
-
 export function main(): void {
   // TODO(snorbi07): read from command line parameters. Right now this is not important as this is not the main usage
   const outputFolder = 'police_hu_date';
-  const fetchTaskCallback = (crossingInfo: Crossings): Promise<boolean> =>
-    saveCurrentPoliceHuCrossingInfo(crossingInfo, outputFolder);
+  const outputFolderRawHTML = 'police_hu_raw_date';
+  const fetchTaskCallback = (crossingInfo: Crossings, rawCrossingInfo: RawCrossingData): Promise<boolean> =>
+    saveCurrentPoliceHuCrossingInfo(crossingInfo, outputFolder, rawCrossingInfo, outputFolderRawHTML);
   startFetchTask(fetchTaskCallback);
   return;
 }
